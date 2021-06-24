c2784d0357baa4e71bf024e15908babf56dbb826
hayeon17kim <hayeon17kim@gmail.com>
2021-06-24 13:57:39 +0900

    add onclick event

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
 
+  // 언제나 2개의 요소를 가진 배열을 리턴한다.
+  // first element : currentStateSnapshot
+  // second element: function that change the value => react will reexecute 
+  // whenever call state changing function react willl reexecute copmoent function and will evalute and update rendering
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
   function deleteHandler() {
-    console.log('deleteHandler');
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
 
   return (
     <div className="card">
       <h2>{ props.text }</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={ deleteHandler }>Delete</button>
       </div>
-    </div>
+      { modalIsOpen && <Modal onCancel={ closeModalHandler } onConfirm={ closeModalHandler }/> }
+      { modalIsOpen && <Backdrop onCancel={ closeModalHandler }/> }
+    </div> 
   );
 }
 
